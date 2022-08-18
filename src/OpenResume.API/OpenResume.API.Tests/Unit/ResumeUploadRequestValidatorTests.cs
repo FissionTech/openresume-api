b437b3cdfbc0b9dfa5eebac15736a44f88c4faaf
FissionTech <michael.lindsay.j@gmail.com>
2022-08-18 09:53:02 -0400

    Added some unit tests for resume upload

---
+++ src/OpenResume.API/OpenResume.API.Tests/Unit/ResumeUploadRequestValidatorTests.cs
@@ -0,0 +1,73 @@
+#nullable enable
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Newtonsoft.Json.Linq;
+using OpenResume.API.Util;
+
+namespace OpenResume.API.Tests.Unit
+{
+    public class ResumeUploadRequestValidatorTests
+    {
+        [Fact]
+        public async Task IsValidFailsOnEmptyString() {
+            var mockRequest = new Mock<HttpRequest>();
+
+            Stream mockBodyStream = mockBodyStream = new MemoryStream();
+            StreamWriter sw = new StreamWriter(mockBodyStream);
+            sw.Write(string.Empty);
+            sw.Flush();
+
+            mockRequest.Setup(req => req.Body).Returns(mockBodyStream);
+
+            ResumeUploadRequestValidator validator = new ResumeUploadRequestValidator();
+            bool success = await validator.IsValid(mockRequest.Object);
+
+            sw.Close();
+
+            Assert.False(success);
+        }
+
+        [Fact]
+        public async Task IsValidSucceedsOnSimpleJsonBody() {
+            var mockRequest = new Mock<HttpRequest>();
+
+            Stream mockBodyStream = mockBodyStream = new MemoryStream();
+            StreamWriter sw = new StreamWriter(mockBodyStream);
+            sw.Write("{\"key\":\"value\"}");
+            sw.Flush();
+            mockBodyStream.Position = 0;
+
+            mockRequest.Setup(req => req.Body).Returns(mockBodyStream);
+
+            ResumeUploadRequestValidator validator = new ResumeUploadRequestValidator();
+            bool success = await validator.IsValid(mockRequest.Object);
+
+            sw.Close();
+
+            Assert.True(success);
+        }
+
+        [Fact]
+        public async Task TryParseSucceedsOnSimpleJsonBody() {
+            var mockRequest = new Mock<HttpRequest>();
+
+            Stream mockBodyStream = mockBodyStream = new MemoryStream();
+            StreamWriter sw = new StreamWriter(mockBodyStream);
+            sw.Write("{\"key\":\"value\"}");
+            sw.Flush();
+            mockBodyStream.Position = 0;
+
+            mockRequest.Setup(req => req.Body).Returns(mockBodyStream);
+
+            ResumeUploadRequestValidator validator = new ResumeUploadRequestValidator();
+            var (success, result, exception) = await validator.TryParse(mockRequest.Object);
+
+            string resultValue = result["key"].ToString();
+            
+            sw.Close();
+
+            Assert.True(resultValue == "value");
+        }
+    }
+}
+#nullable disable
